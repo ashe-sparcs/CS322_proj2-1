743825030c53e4e1d4660d6ba0e3f4476c8c1d90
ashe-sparcs <wotjd1102@gmail.com>
2016-11-08 05:27:13 +0900

    e_closure complete. initial state changed type(string to string list)

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ class ENfa:
     symbol = []
     func_dict = {}
     func_string_list = []
-    initial = ''
+    initial = []
     final = []
+    todo_queue = []
+    done_list = []
+    done_dict = {}
 
     def __init__(self, state, symbol, func_string_list, initial, final):
         self.state = state
@@ -19,11 +22,28 @@ class ENfa:
         for func_string in func_string_list:
             func_string_split = func_string.split(',')
             self.func_dict[func_string_split[0]][func_string_split[1]] = func_string_split[2]
-        self.initial = initial
+        self.initial = list(initial)
         self.final = final
+        self.todo_queue = self.e_closure(self.initial)
+        self.done_list = list(self.todo_queue)
 
     def transition(self, from_state, input_symbol):
         return self.func_dict[from_state][input_symbol]
+
+    def e_closure(self, substate):
+        result = list(substate)
+        for ss in substate:
+            result.append(self.transition(ss, 'E'))
+        return result
+
+    def convert_to_dfa(self):
+        while self.todo_queue:
+            from_substate = self.todo_queue.pop(0)
+            to_substate = []
+            for fs in from_substate:
+                for sym in self.symbol:
+                    to_substate.append(self.transition(fs, sym))
+            to_substate = self.e_closure(to_substate)
 
     def print_self(self):
         print('State')
@@ -39,7 +59,7 @@ class ENfa:
             for sym in sorted(list(self.func_dict[q])):
                 print(q + ',' + sym + ',' + self.transition(q, sym))
         print('Initial state')
-        print(self.initial)
+        print(','.join(self.initial))
         print('Final state')
         print(','.join(self.final))
 
@@ -49,8 +69,12 @@ class Dfa(ENfa):
         ENfa.__init__(self, state, symbol, func_string_list, initial, final)
         for key in list(self.func_dict.keys()):
             del self.func_dict[key]['E']
-        self.initial = initial
-        self.final = final
+
+    def rename(self):
+        pass
+
+    def minimize(self):
+        pass
 
 
 def main():
@@ -71,14 +95,16 @@ def main():
             break
         func_string_list.append(line_temp)
     # Initial State
-    q0 = dfa_file.readline().strip()
+    q0 = dfa_file.readline().strip().split(',')
     dfa_file.readline()  # Final State
     f = dfa_file.readline().strip().split(',')
 
-    dfa = Dfa(q, sigma, func_string_list, q0, f)
-    dfa.print_self()
+    # dfa = Dfa(q, sigma, func_string_list, q0, f)
+    # dfa.print_self()
+
+    e_nfa = ENfa(q, sigma, func_string_list, q0, f)
+    print(e_nfa.e_closure(e_nfa.initial))
 
 
 main()
-
 
